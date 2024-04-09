6e5c5553e2bbbe921edeb734a59e5ed4fed1b88a
Shreeharinadig <93840189+Shreeharinadig@users.noreply.github.com>
2024-04-09 23:00:03 +0530

    Delete src/main/java/com/example/financetracker/service/BudgetRepository.java

--- src/main/java/com/example/financetracker/service/BudgetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.financetracker.repository;
-
-import com.example.financetracker.model.Budget;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BudgetRepository extends MongoRepository<Budget, String> {
-}
-
