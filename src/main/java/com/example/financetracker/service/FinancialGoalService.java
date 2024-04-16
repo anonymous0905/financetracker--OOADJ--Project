ccc73351cd0a2a80fc01f2806ed6ebbff2fc9f4c
anonymous0905 <pes2202100762@pesu.pes.edu>
2024-04-16 21:20:32 +0530

    financial goal (facade pattern)

--- src/main/java/com/example/financetracker/service/FinancialGoalService.java
+++ src/main/java/com/example/financetracker/service/FinancialGoalService.java
@@ -1,30 +1,3 @@
-// package com.example.financetracker.service;
-
-// import com.example.financetracker.model.FinancialGoal;
-// import com.example.financetracker.repository.FinancialGoalRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// import java.util.List;
-
-// @Service
-// public class FinancialGoalService {
-//     private final FinancialGoalRepository financialGoalRepository;
-
-//     @Autowired
-//     public FinancialGoalService(FinancialGoalRepository financialGoalRepository) {
-//         this.financialGoalRepository = financialGoalRepository;
-//     }
-
-//     public List<FinancialGoal> findAllFinancialGoals() {
-//         return financialGoalRepository.findAll();
-//     }
-
-//     public FinancialGoal addFinancialGoal(FinancialGoal financialGoal) {
-//         return financialGoalRepository.save(financialGoal);
-//     }
-// }
-
 
 package com.example.financetracker.service;
 
